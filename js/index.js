0f5ea4c4db19ffba123b1c15305818500756173e
Olena Salnikova <nameless.hire5@gmail.com>
2026-03-03 18:30:04 -0500

    Homework 13 - API Project

--- js/index.js
+++ js/index.js
@@ -13,14 +13,61 @@ footer.appendChild(copyright);                               // Add the copyrigh
 
 
 // ---- Fetching GitHub Repositories ----
-
-fetch("https://api.open-meteo.com/v1/forecast?latitude=41.4995&longitude=-81.6954&daily=sunrise,sunset&hourly=temperature_2m&current=temperature_2m&timezone=America%2FNew_York&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch")
+timeZone = "America/New_York";
+fetch(`https://api.open-meteo.com/v1/forecast?latitude=41.4995&longitude=-81.6954&daily=sunrise,sunset&hourly=temperature_2m&current=temperature_2m&timezone=${timeZone}&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`)
     .then(response => response.json())
     .then(weather => {
         console.log(weather);
 
-        const projectSection = document.querySelector("#api");
-        const projectList = projectSection.querySelector("ul");
+        const projectSection = document.querySelector("#api"); // Select the projects section by id
+        const projectList = projectSection.querySelector("ul");   // Select the <ul> inside the projects section
+
+        projectList.innerHTML = ""; // Clear any existing content in the project list
+
+        // --- Current Temperature ---
+        const currentTemp = document.createElement("li");
+        let icon = {
+            src: "images/warm-temp.png",
+            alt: "Warm Icon"
+        };
+        if (weather.current.temperature_2m >= 80) {
+            icon.src = "images/hot-temp.png";
+            icon.alt = "Hot Icon";
+        } else if (weather.current.temperature_2m <= 32) {
+            icon.src = "images/cold-temp.png";
+            icon.alt = "Cold Icon";
+        }
+        
+        let tempIcon;
+        tempIcon = document.createElement("img");
+        tempIcon.src = icon.src;
+        tempIcon.alt = icon.alt;
+        currentTemp.appendChild(tempIcon);
+
+        currentTemp.appendChild(document.createTextNode(`Current Temperature: ${weather.current.temperature_2m}°F`));
+        projectList.appendChild(currentTemp);
+
+        // --- Sunrise ---
+        const sunrise = document.createElement("li");
+        const sunriseIcon = document.createElement("img");
+        sunriseIcon.src = "images/sunrise.png";
+        sunriseIcon.alt = "Sunrise Icon";
+        sunrise.appendChild(sunriseIcon);
+        sunrise.appendChild(document.createTextNode(`Sunrise: ${weather.daily.sunrise[0]} `));
+        sunrise.appendChild(document.createElement("br"));
+        sunrise.appendChild(document.createTextNode(timeZone));
+        projectList.appendChild(sunrise);
+
+        // --- Sunset ---
+        const sunset = document.createElement("li");
+        const sunsetIcon = document.createElement("img");
+        sunsetIcon.src = "images/sunset.png";
+        sunsetIcon.alt = "Sunset Icon";
+        sunset.appendChild(sunsetIcon);
+        sunset.appendChild(document.createTextNode(`Sunset: ${weather.daily.sunset[0]} `));
+        sunset.appendChild(document.createElement("br"));
+        sunset.appendChild(document.createTextNode(timeZone));
+        projectList.appendChild(sunset);
     })
     .catch(error =>
         console.error("Error fetching weather data:", error)
